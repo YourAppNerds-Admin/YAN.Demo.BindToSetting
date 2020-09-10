3a72e411ed4f146e79eb2fd538582191870a17ba
Kyle <kyle@yourappnerds.com>
2020-09-09 20:59:32 -0400

    Add project files.

---
+++ YAN.Demo.BindToSetting/StringCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace YAN.Demo.BindToSetting
+{
+    public static class StringCollectionExtensions 
+    {
+        public static ObservableCollection<string> ToObservableStringCollection(this StringCollection Source)
+        {
+            ObservableCollection<string> Collection = new ObservableCollection<string>();
+            foreach (var Item in Source)
+            {
+                Collection.Add(Item);
+            }
+            return Collection;
+        }
+
+        public static StringCollection ToStringCollection(this ObservableCollection<string> Source)
+        {
+            StringCollection Collection = new StringCollection();
+            foreach (var Item in Source)
+            {
+                Collection.Add(Item);
+            }
+            return Collection;
+        }
+
+        public static ObservableCollection<ListviewCheckboxItem> ToObservableListviewCheckboxCollection(this StringCollection Source)
+        {
+            ObservableCollection<ListviewCheckboxItem> Collection = new ObservableCollection<ListviewCheckboxItem>();
+            for (int Index = 0; Index < Source.Count; Index += 2)
+            {
+                Collection.Add(new ListviewCheckboxItem(Source[Index], Convert.ToBoolean(Source[Index + 1])));
+            }
+            return Collection;
+        }
+
+        public static StringCollection ToStringCollection(this ObservableCollection<ListviewCheckboxItem> Source)
+        {
+            StringCollection Collection = new StringCollection();
+            foreach (var Item in Source)
+            {
+                Collection.Add(Item.Text);
+                Collection.Add(Item.IsChecked.ToString());
+            }
+            return Collection;
+        }
+    }
+}
---
+++ YAN.Demo.BindToSetting/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace YAN.Demo.BindToSetting.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string PropertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+        private Properties.Settings Settings => Properties.Settings.Default;
+
+        public string StringSetting
+        {
+            get => Settings.StringSetting;
+            set
+            {
+                Settings.StringSetting = value;
+                Settings.Save();
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> StringCollectionSetting
+        {
+            get => Settings.StringCollectionSetting.ToObservableStringCollection();
+            set
+            {
+                Settings.StringCollectionSetting = value.ToStringCollection();
+                Settings.Save();
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<ListviewCheckboxItem> StringCollectionToObjectSetting
+        {
+            get => Settings.StringCollectionToObjectSetting.ToObservableListviewCheckboxCollection();
+            set
+            {
+                Settings.StringCollectionToObjectSetting = value.ToStringCollection();
+                Settings.Save();
+                OnPropertyChanged();
+            }
+        }
+    }
+}
